da4e414819886730cb26e7cd4e12e56cb6315e40
Ankit Gochhayat <146383416+ankitg-02@users.noreply.github.com>
2024-01-16 19:27:02 +0530

    Add files via upload

---
+++ ll.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct  node
+{
+    int data;
+    struct  node*next;
+};
+void main(){
+    struct node *head,*newnode,*temp;
+    head=NULL;
+    int choice=1;
+    while(choice==1){
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("Enter data:");
+        scanf("%d",&newnode->data);
+        newnode->next=NULL;
+        if(head=NULL){
+            head=temp=newnode;
+        } 
+        else{
+            temp->next=newnode;
+            temp=newnode;
+        }
+        printf("Do you want to continue(0/1)");
+        scanf("%d",&choice);
+    }
+    temp=head;
+    while(temp!= NULL)
+    {
+        printf("%d",temp->data);
+        temp=temp->next;
+    }    
+}
